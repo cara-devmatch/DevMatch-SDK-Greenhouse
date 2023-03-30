17594dc3c494beec0bf6a38b7cbdd89e52761db9
Alan Gonzalez <alanboy@alanboy.net>
2023-03-30 13:30:16 -0700

    latest changes from surface go

--- validator/dist/validator.js
+++ validator/dist/validator.js
@@ -1,27 +1,35 @@
 "use strict";
 Object.defineProperty(exports, "__esModule", { value: true });
 exports.Validator = void 0;
-/**
- *
- *
- **/
+const interfaces_1 = require("./interfaces");
 class Validator {
     getTestCases() {
         return [];
     }
     prerequesites(user) {
+        // There are no pre-requisites for this problem.
         return true;
     }
     getProblemStatement(userId) {
+        return `In this assessment you need to do the follwing:
+
+* First, do this.
+* Then do that.
+
+That's it!
+`;
     }
     getProblemConfiguration() {
-        return new ProblemConfiguration();
+        let config = new interfaces_1.ProblemConfiguration();
+        config.ideEnabled = false;
+        config.inputType = interfaces_1.ProblemInputType.GitRepo;
+        return config;
     }
     openProblem(user) {
-        return new ProblemOpenedResult();
+        return Promise.resolve(new interfaces_1.ProblemOpenedResult());
     }
     validate(id, user, testCases, databag, validationInput) {
-        return testCases;
+        return Promise.resolve(testCases);
     }
 }
 exports.Validator = Validator;
--- validator/src/validator.ts
+++ validator/src/validator.ts
@@ -1,30 +1,70 @@
-import { DevMatchValidator, ProblemTestCase } from './interfaces'
+import { DevMatchValidator, EvaluatedTestCase, ProblemConfiguration, ProblemInputType, ProblemOpenedResult, ProblemTestCase, User } from './interfaces'
 
-/**
- *
- *
- **/
 export class Validator implements DevMatchValidator{
     getTestCases(): ProblemTestCase[] {
-        return [];
+        return [
+            new ProblemTestCase({
+            id: "TEST_1",
+            description: "Add two integers",
+            maxPoints: 10,
+        }),
+        new ProblemTestCase({
+            id: "TEST_2",
+            description: "Bad arguments - Too few",
+            maxPoints: 10,
+        }),
+        new ProblemTestCase({
+            id: "TEST_3",
+            description: "Bad arguments - Too many",
+            maxPoints: 10,
+        }),
+        ]
     }
 
     prerequesites(user: User) {
+        // There are no pre-requisites for this problem.
         return true;
     }
 
     getProblemStatement(userId: string): string {
+        return  `In this assessment you need to do the follwing:
+
+* First, do this.
+* Then do that.
+
+That's it!
+`
+
     }
 
+    /**
+     * 
+     * @returns An instance of the configuration object
+     */
     getProblemConfiguration(): ProblemConfiguration {
-        return new ProblemConfiguration();
+        let config = new ProblemConfiguration();
+        config.ideEnabled = false;
+        config.inputType = ProblemInputType.GitRepo;
+        return config;
     }
 
+    /**
+     * 
+     * @param user The user that is opening this problem
+     * @returns A ProblemOpenedResult with information about the action of opening.
+     */
     openProblem(user: User): Promise<ProblemOpenedResult> {
-        return new ProblemOpenedResult()
+        let openResult = new ProblemOpenedResult();
+        openResult.opened = true;
+        openResult.databag.set('date', new Date().getTime().toString())
+        openResult.instructions = `
+These are instructions.`
+
+        return Promise.resolve(openResult)
     }
+
 
     validate( id: number, user: User, testCases: EvaluatedTestCase[], databag: Map<string, string>, validationInput?: any,): Promise<EvaluatedTestCase[]> {
-        return testCases;
+        return Promise.resolve(testCases);
     }
 }
