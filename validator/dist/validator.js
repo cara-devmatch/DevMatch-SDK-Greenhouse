74dac6c3af6dcbdd9715d782963dd49ca99784dd
Alan Gonzalez <alanboy@alanboy.net>
2023-04-03 13:57:04 -0700

    ignoring dist

--- validator/dist/validator.js
+++ validator/dist/validator.js
@@ -4,7 +4,23 @@ exports.Validator = void 0;
 const interfaces_1 = require("./interfaces");
 class Validator {
     getTestCases() {
-        return [];
+        return [
+            new interfaces_1.ProblemTestCase({
+                id: "TEST_1",
+                description: "Add two integers",
+                maxPoints: 10,
+            }),
+            new interfaces_1.ProblemTestCase({
+                id: "TEST_2",
+                description: "Bad arguments - Too few",
+                maxPoints: 10,
+            }),
+            new interfaces_1.ProblemTestCase({
+                id: "TEST_3",
+                description: "Bad arguments - Too many",
+                maxPoints: 10,
+            }),
+        ];
     }
     prerequesites(user) {
         // There are no pre-requisites for this problem.
@@ -19,14 +35,28 @@ class Validator {
 That's it!
 `;
     }
+    /**
+     *
+     * @returns An instance of the configuration object
+     */
     getProblemConfiguration() {
         let config = new interfaces_1.ProblemConfiguration();
         config.ideEnabled = false;
         config.inputType = interfaces_1.ProblemInputType.GitRepo;
         return config;
     }
+    /**
+     *
+     * @param user The user that is opening this problem
+     * @returns A ProblemOpenedResult with information about the action of opening.
+     */
     openProblem(user) {
-        return Promise.resolve(new interfaces_1.ProblemOpenedResult());
+        let openResult = new interfaces_1.ProblemOpenedResult();
+        openResult.opened = true;
+        openResult.databag.set('date', new Date().getTime().toString());
+        openResult.instructions = `
+These are instructions.`;
+        return Promise.resolve(openResult);
     }
     validate(id, user, testCases, databag, validationInput) {
         return Promise.resolve(testCases);
