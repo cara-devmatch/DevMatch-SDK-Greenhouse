e23b48c99742b6f6217d415ff4d1583dd0f6c594
Alan Gonzalez <alanboy@alanboy.net>
2023-04-11 15:20:58 -0700

    building with webpack

---
+++ validator/webpack.config.js
@@ -0,0 +1,30 @@
+//webpack.config.js
+const path = require('path');
+
+module.exports = {
+  // production or development
+  mode: "development",
+
+  // inline-source-map -> Possible choice when publishing a single file
+  // (none) -> Recommended choice for production builds with maximum performance.
+  devtool: "inline-source-map",
+  entry: {
+    main: "./src/validator.ts",
+  },
+  output: {
+    library: 'mylib',
+    path: path.resolve(__dirname, './dist'),
+    filename: "[name]-bundle.js" // <--- Will be compiled to this single file
+  },
+  resolve: {
+    extensions: [".ts", ".tsx", ".js"],
+  },
+  module: {
+    rules: [
+      { 
+        test: /\.tsx?$/,
+        loader: "ts-loader"
+      }
+    ]
+  }
+};
