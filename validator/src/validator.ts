f0ab7669caae579b6554d3f5305eb9377fabaf6f
Cara Lisy <cara@devmatch.io>
2023-10-04 18:56:32 -0700

    workspace

--- validator/src/validator.ts
+++ validator/src/validator.ts
@@ -36,36 +36,35 @@ export class Validator implements DevMatchValidator {
     }
 
     async getProblemStatement(userId: string): Promise<string> {
-        return Promise.resolve(`{diff}`)
-        // return Promise.resolve(
-// `# The Greenhouse
+        return Promise.resolve(
+`# The Greenhouse
 
-// Your friends own a large greenhouse, and recently set up some great technology that can care for plants at the push of a button! They have put together an API so they can interact with their new technology from afar using Python's FastAPI framework and SQL. You've been assigned to review a recent pull request to the API that modifies some core functionality.
+Your friends own a large greenhouse, and recently set up some great technology that can care for plants at the push of a button! They have put together an API so they can interact with their new technology from afar using Python's FastAPI framework and SQL. You've been assigned to review a recent pull request to the API that modifies some core functionality.
 
-// # The Database
+# The Database
 
-// ## plants table
+## plants table
 
-// Plants have an \`id (primary key)\`, \`name (varchar)\` (your friends treat their plants like family), \`species (varchar)\`, \`sell_price (float)\`, and \`watering_interval_days (int)\` (which indicates how often that plant need watered).
+Plants have an \`id (primary key)\`, \`name (varchar)\` (your friends treat their plants like family), \`species (varchar)\`, \`sell_price (float)\`, and \`watering_interval_days (int)\` (which indicates how often that plant need watered).
 
-// ## watering_events table
+## watering_events table
 
-// This table keeps track of when plants were last watered. A watering event has a \`plant_id (foreign key to plant's id)\` and an \`event_date (datetime)\`.
+This table keeps track of when plants were last watered. A watering event has a \`plant_id (foreign key to plant's id)\` and an \`event_date (datetime)\`.
 
-// ## sales_events table
+## sales_events table
 
-// This table keeps track of when plants were sold. a sales event has a \`plant_id (foreign key to plant's id)\`, a \`customer_name (varchar)\`, and an \`event_date (datetime)\`.
+This table keeps track of when plants were sold. a sales event has a \`plant_id (foreign key to plant's id)\`, a \`customer_name (varchar)\`, and an \`event_date (datetime)\`.
 
-// Sold plants do not need watered anymore because they are no longer in the greenhouse.
+Sold plants do not need watered anymore because they are no longer in the greenhouse.
 
-// # Changes For Review
+# Changes For Review
 
-// Selling plants is a new operation for your friends, so the \`sell_price\` field on the \`plants\` table is a new column. Existing endpoints need to reflect this change, and a new endpoint was added to initate a sales event.
+Selling plants is a new operation for your friends, so the \`sell_price\` field on the \`plants\` table is a new column. Existing endpoints need to reflect this change, and a new endpoint was added to initate a sales event.
 
-// Review the changes made, providing feedback on this pull request including readability, maintainability, and considering edge cases in inputs.
+Review the changes made, providing feedback on this pull request including readability, maintainability, and considering edge cases in inputs.
 
-// (The changes are being merged from the \`after-changes\` branch to the \`main\` branch of the repo at \`{repoUrl}\`.)
-// `)
+(The changes are being merged from the \`after-changes\` branch to the \`main\` branch of the repo at \`{repoUrl}\`.)
+`)
     }
 
     /**
@@ -83,17 +82,6 @@ export class Validator implements DevMatchValidator {
      */
     async openProblem(user: User): Promise<ProblemOpenedResult> {
         let openResult = new ProblemOpenedResult();
-        const repoUrl = "https://github.com/cara-devmatch/Greenhouse-Code.git"
-        const diff = `diff --git a/sample.js b/sample.js
-index 0000001..0ddf2ba
---- a/sample.js
-+++ b/sample.js
-@@ -1 +1 @@
--console.log("Hello World!")
-+console.log("Hello from Diff2Html!")`
-
-        openResult.databag.set("diff", diff)
-        openResult.databag.set("repoUrl", repoUrl)
 
         openResult.opened = true;
         openResult.databag.set('date', new Date().getTime().toString())
